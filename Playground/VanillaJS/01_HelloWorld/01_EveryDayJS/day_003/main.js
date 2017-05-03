a486a61f786032762598cdd6a90d577561475594
Florin Asavei <asaveiflorin@gmail.com>
2017-05-03 19:10:05 +0300

    renaming

---
+++ Playground/VanillaJS/01_HelloWorld/01_EveryDayJS/day_003/main.js
@@ -0,0 +1,17 @@
+console.log('Hello World');
+
+var nume;
+console.log("nume:", nume);
+
+nume = 1;
+var cutie = "pantofi";
+nume = cutie;
+var flag = true;
+var sir = ["eu", "tu", 5, nume, cutie, flag];
+nume = 1;
+
+console.log("nume: " + nume);
+// console.log("cutie: " + cutie);
+// console.log("sir: " , sir);
+// console.log("sir[0]: " , sir[0]);
+// console.log("lungime sir: " , sir.length);
