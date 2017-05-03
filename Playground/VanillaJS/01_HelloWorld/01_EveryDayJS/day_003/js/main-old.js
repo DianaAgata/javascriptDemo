15fa6550a7434cdcb362f3721da33fd702f4fe62
Florin Asavei <asaveiflorin@gmail.com>
2017-05-03 20:12:38 +0300

    listeners example

---
+++ Playground/VanillaJS/01_HelloWorld/01_EveryDayJS/day_003/js/main-old.js
@@ -0,0 +1,43 @@
+// function firstFunction(x, y) {
+//   console.log("x when entering function: ", x);
+//   console.log("y when entering function: ", y);
+//   return 2 * x + y + 1;
+// }
+//
+// console.log(firstFunction(2, 7));
+//
+// var vf = firstFunction(3, 8) + 1;
+//
+// console.log(vf);
+
+
+function displayMessage(msg) {
+  console.log("=====");
+  console.log(msg);
+  console.log("-----");
+}
+
+displayMessage('message1');
+displayMessage('message2');
+
+
+
+// console.log('Hello World');
+//
+// var nume;
+//
+// console.log("nume:", nume);
+//
+// nume = 1;
+// var cutie = "pantofi";
+// nume = cutie;
+// var flag = true;
+// var sir = ["eu", "tu", 5, nume, cutie, flag];
+// nume = 1;
+// var nume1 = 1 + nume;
+// console.log("nume: " + nume);
+// console.log("nume1: " + nume1);
+// console.log("cutie: " + cutie);
+// console.log("sir: " , sir);
+// console.log("sir[0]: " , sir[0]);
+// console.log("lungime sir: " , sir.length);
---
+++ Playground/VanillaJS/01_HelloWorld/01_EveryDayJS/day_003/js/main.js
@@ -0,0 +1,6 @@
+var p1Elem = document.getElementById("p1");
+console.dir(p1Elem);
+
+p1Elem.addEventListener("click", function() {
+  console.log('clicked');
+});
