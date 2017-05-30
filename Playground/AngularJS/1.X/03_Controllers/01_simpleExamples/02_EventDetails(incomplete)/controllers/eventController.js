4ab0cb8fb9cfb9cdbec01df4b64ffbb4b3d27469
Asavei Florin <asaveiflorin@gmail.com>
2017-05-30 23:12:13 +0300

    ng work

--- Playground/AngularJS/1.X/03_Controllers/01_simpleExamples/02_EventDetails(incomplete)/controllers/eventController.js
+++ Playground/AngularJS/1.X/03_Controllers/01_simpleExamples/02_EventDetails(incomplete)/controllers/eventController.js
@@ -1,7 +1,11 @@
 'use strict';
 
-eventsApp.controller('EventController',
-    function EventController() {
-
-    }
+eventsApp.controller('EventController', ["$scope",
+    function EventController($scope) {
+        $scope.event = {
+            "name": "Awesome Event",
+            "date": "24/05/2017",
+            "time": "10pm"
+        };
+    }]
 );
