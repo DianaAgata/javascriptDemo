22cf7f4a2309dc896158494379a7a686cb4fe5bf
Asavei Florin <asaveiflorin@gmail.com>
2017-06-05 13:31:17 +0300

    adding icons to the app

--- Playground/AngularJS/1.X/Demos/app03_Codeshool-Gems/app.js
+++ Playground/AngularJS/1.X/Demos/app03_Codeshool-Gems/app.js
@@ -1,36 +1,40 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  var app = angular.module('store', []);
+    var app = angular.module('store', []);
 
-  app.controller('StoreController', function() {
-    this.products = gems;
-  });
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
 
-  var gems = [{
-    name: 'Awesomeness',
-    price: 25,
-    description: 'this is awesome',
-    isSoldOut: false,
-    canPuchse: true
-  }, {
-    name: 'Cool',
-    price: 35,
-    description: 'this is cool',
-    isSoldOut: false,
-    canPuchse: true
-  }, {
-    name: 'Sweet',
-    price: 30,
-    description: 'this is sweet',
-    isSoldOut: true,
-    canPuchse: true
-  }, {
-    name: 'Meh',
-    price: 20,
-    description: 'this is meh',
-    isSoldOut: true,
-    canPuchse: true
-  }];
+    var gems = [{
+        name: 'Wind',
+        price: 25,
+        description: 'this is awesome',
+        isSoldOut: false,
+        canPurchase: true,
+        image: "img/icons/_PNG64/weather_wind.png"
+    }, {
+        name: 'Hail',
+        price: 35,
+        description: 'this is cool',
+        isSoldOut: false,
+        canPurchase: true,
+        image: "img/icons/_PNG64/weather_hail.png"
+    }, {
+        name: 'Moon',
+        price: 30,
+        description: 'this is sweet',
+        isSoldOut: false,
+        canPurchase: true,
+        image: "img/icons/_PNG64/weather_moon.png"
+    }, {
+        name: 'Rain',
+        price: 20,
+        description: 'this is meh',
+        isSoldOut: true,
+        canPurchase: true,
+        image: "img/icons/_PNG64/weather_rain.png"
+    }];
 
 })();
