fa3d1f460af2f12c8b2644c087b5b1872f8deca1
Asavei Florin <asaveiflorin@gmail.com>
2017-06-15 22:41:22 +0300

    sync

--- Playground/VanillaJS/Tutorials/01_EveryDayJS/day_009/app.js
+++ Playground/VanillaJS/Tutorials/01_EveryDayJS/day_009/app.js
@@ -7,10 +7,32 @@ var winner = document.getElementById('winner');
 var extractBtn = document.getElementById('extract-btn');
 
 addPersonButn.addEventListener("click", function() {
-  if(addPerson.value.length){
+  if (addPerson.value.length) {
     addPerson.style.borderColor = "initial";
-    participants.push(addPerson.value)
-  }else{
+    participants.push(addPerson.value);
+    console.log(participants);
+
+    var paragraph = document.createElement("P");
+    var text = document.createTextNode(addPerson.value);
+    paragraph.appendChild(text);
+    participantsList.appendChild(paragraph);
+    addPerson.value = "";
+  } else {
     addPerson.style.borderColor = "red";
   }
+});
+
+extractBtn.addEventListener("click", function() {
+  function getRandomInt(number) {
+    return Math.floor(Math.random() * (number + 1));
+  }
+
+  if(participants.length){
+    var random = getRandomInt(participants.length - 1);
+    winner.innerHTML = participants[random];
+  }else{
+    extractBtn.className = "btn btn-danger";
+  }
+
+
 });
