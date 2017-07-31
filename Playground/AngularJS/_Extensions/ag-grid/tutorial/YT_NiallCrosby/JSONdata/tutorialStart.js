b82d3b11720dcaeebad283b5ae7aa96385221f9d
Florin Asavei <asaveiflorin@gmail.com>
2017-07-31 13:23:05 +0300

    getting data from local json

---
+++ Playground/AngularJS/_Extensions/ag-grid/tutorial/YT_NiallCrosby/JSONdata/tutorialStart.js
@@ -0,0 +1,46 @@
+'use strict';
+
+(function () {
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+        var gridDiv = document.querySelector('#myGrid');
+
+        var gridOptions = {
+            columnDefs: [
+                {headerName: 'Country', field: 'Country'},
+                {headerName: 'City', field: 'City'}
+
+            ]
+        };
+
+        new agGrid.Grid(gridDiv, gridOptions);
+
+        jsonLoad(function (data) {
+            gridOptions.api.setRowData(data);
+            console.log('got data', data);
+        });
+
+        console.log('gridDiv = ', gridDiv);
+    });
+
+})();
+
+
+function jsonLoad(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'MOCK_DATA.json'); //by default async
+    xhr.responseType = 'json';
+
+    xhr.onload = function () {
+        if (this.status == 200) { //onload called even on 404 etc so check the status
+            callback(this.response);
+        }
+    };
+
+    xhr.onerror = function () {
+        console.log('loading data error');
+    };
+
+    xhr.send();
+}
