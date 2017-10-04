42b66c318499ca053cda53b60c9c8f4b28104767
Asavei Florin <asaveiflorin@gmail.com>
2017-10-04 21:51:13 +0300

    learning about prommises

---
+++ Playground/VanillaJS/03_Concepts/Promises/tutorial01.js
@@ -0,0 +1,23 @@
+console.log('start program');
+
+let promiseToCleanTheRoom = new Promise(
+    function(resolve,reject){
+
+      //cleaning the room
+
+      let isClean = true;
+
+      if(isClean){
+          resolve('Clean');
+      }else{
+          reject('not Clean');
+      }
+
+    }
+);
+
+promiseToCleanTheRoom.then(function(fromResolve){
+    console.log('the room is ' + fromResolve);
+}).catch(function(fromReject){
+    console.log('the room is ' + fromReject);
+});
