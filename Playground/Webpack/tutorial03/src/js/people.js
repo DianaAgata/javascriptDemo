358964d6519c849b42627f2ff2e60e0b41ff732d
Asavei Florin <asaveiflorin@gmail.com>
2017-10-06 21:35:44 +0300

    functioning webpack boilerplate

---
+++ Playground/Webpack/tutorial03/src/js/people.js
@@ -0,0 +1,11 @@
+// function getHello(){
+//     return 'Hello There';
+// }
+
+let people = [
+    {name: 'Harrold Show'},
+    {name: 'Jane Done'},
+    {name: 'Carol Smith'}
+];
+
+module.exports = people;
