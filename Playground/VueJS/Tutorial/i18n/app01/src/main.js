c93e323542ac8e2dbea740fb9b259abc5b370be9
Asavei Florin <asaveiflorin@gmail.com>
2017-10-07 21:55:19 +0300

    translations now working

--- Playground/VueJS/Tutorial/i18n/app01/src/main.js
+++ Playground/VueJS/Tutorial/i18n/app01/src/main.js
@@ -5,23 +5,25 @@ import App from './App'
 import router from './router'
 
 import VueI18n from 'vue-i18n'
+import translations from './assets/data/translations/i18n.json'
+
+console.log('translations', translations)
+
+// var axios = require('axios')
+
+// axios.get('http://localhost:8080/data/i18n.json')
+//   .then(function (response) {
+//     console.log('the response: ', response)
+//   })
+//   .catch(function (error) {
+//     console.log(error.message)
+//   })
 
 Vue.use(VueI18n)
 
 Vue.config.productionTip = false
 
-const messages = {
-  en: {
-    message: {
-      hello: 'hello world'
-    }
-  },
-  ro: {
-    message: {
-      hello: 'Salut!'
-    }
-  }
-}
+const messages = translations
 
 const i18n = new VueI18n({
   locale: 'ro', // set locale
@@ -34,5 +36,5 @@ new Vue({
   router,
   i18n,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
