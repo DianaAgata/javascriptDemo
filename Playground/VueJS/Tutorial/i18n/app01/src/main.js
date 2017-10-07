aa0fc77198547e74e9f8a1a2aadeb4003fb10441
Asavei Florin <asaveiflorin@gmail.com>
2017-10-07 20:58:58 +0300

    some more examples i18n

---
+++ Playground/VueJS/Tutorial/i18n/app01/src/main.js
@@ -0,0 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+
+Vue.config.productionTip = false
+
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  ro: {
+    message: {
+      hello: 'Salut!'
+    }
+  }
+}
+
+const i18n = new VueI18n({
+  locale: 'ro', // set locale
+  messages // set locale messages
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  i18n,
+  template: '<App/>',
+  components: { App }
+})
