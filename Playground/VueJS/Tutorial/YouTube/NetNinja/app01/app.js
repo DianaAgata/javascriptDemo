d0baab929be76acc3902a4928dc606a288bdc497
Asavei Florin <asaveiflorin@gmail.com>
2017-10-08 16:20:14 +0300

    more bindings

--- Playground/VueJS/Tutorial/YouTube/NetNinja/app01/app.js
+++ Playground/VueJS/Tutorial/YouTube/NetNinja/app01/app.js
@@ -2,10 +2,12 @@ new Vue({
     el: "#vue-app",
     data: {
         name: "Florin",
-        job: "Ninja"
+        job: "Ninja",
+        website: 'http://www.netninja.co.uk',
+        websiteTag: '<a href="http://www.netninja.co.uk">Net Ninja</a>'
     },
-    methods:{
-        greet: function(time){
+    methods: {
+        greet: function (time) {
             return 'Good ' + time + ' ' + this.name + ' !';
         }
     }
