3fc71847a762f251bd93afcbbbc25252dfe20da9
Asavei Florin <asaveiflorin@gmail.com>
2017-10-27 22:39:59 +0300

    renaming -adding event listeners in vue

---
+++ Playground/VueJS/Tutorial/YouTube/NetNinja/02_events/app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: "#vue-app",
+    data: {
+        name: "Florin",
+        job: "Ninja",
+        age: 26,
+        website: 'http://www.netninja.co.uk',
+        websiteTag: '<a href="http://www.netninja.co.uk">Net Ninja</a>'
+    },
+    methods: {
+        greet: function (time) {
+            return 'Good ' + time + ' ' + this.name + ' !';
+        }
+    }
+
+});
