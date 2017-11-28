d165788a43bc8d0347161f367ecdd2d463de5e66
Florin Asavei <asaveiflorin@gmail.com>
2017-11-28 12:45:17 +0200

    some styling

---
+++ Playground/_otherLibraries/diagrams/03_cytoscape.js/examples/03_layout/02_random/app.js
@@ -0,0 +1,60 @@
+var cy = cytoscape({
+    container: document.getElementById('cy'),
+    elements: [
+        // nodes
+        { data: { id: 'a' } },
+        { data: { id: 'b' } },
+        { data: { id: 'c' } },
+        { data: { id: 'd' } },
+        { data: { id: 'e' } },
+        { data: { id: 'f' } },
+        // edges
+        {
+            data: {
+                id: 'ab',
+                source: 'a',
+                target: 'b'
+            }
+        },
+        {
+            data: {
+                id: 'cd',
+                source: 'c',
+                target: 'd'
+            }
+        },
+        {
+            data: {
+                id: 'ef',
+                source: 'e',
+                target: 'f'
+            }
+        },
+        {
+            data: {
+                id: 'ac',
+                source: 'a',
+                target: 'c'
+            }
+        },
+        {
+            data: {
+                id: 'be',
+                source: 'b',
+                target: 'e'
+            }
+        }
+    ],
+    layout: {
+        name: 'random'
+    },
+    style: [
+        {
+            selector: 'node',
+            style: {
+                shape: 'hexagon',
+                'background-color': 'red',
+                label: 'data(id)'
+            }
+        }]
+});
