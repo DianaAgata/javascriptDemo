6160b43ca7c10b358f1488f37b5f8b73a034cbac
Asavei Florin <asaveiflorin@gmail.com>
2017-12-01 20:06:03 +0200

    basically copying and pasting

--- Playground/VueJS/Tutorial/Udemy/TODOapp/src/index.js
+++ Playground/VueJS/Tutorial/Udemy/TODOapp/src/index.js
@@ -1,1 +1,24 @@
-console.log('hello World');
+import Vue from 'vue';
+import App from './App'
+
+
+new Vue({
+    el: '#app',
+    template: `<App :state="state" />`,
+    components: { App },
+    data:{
+        state:{
+            showDone: false,
+            todos:[{
+                text: "Refactor CSS",
+                done: true,
+            },{
+                text: "Secure Docker Instances",
+                done: false
+            },{
+                text: "Inject flavoring",
+                done: false
+            }]
+        }
+    }
+});
