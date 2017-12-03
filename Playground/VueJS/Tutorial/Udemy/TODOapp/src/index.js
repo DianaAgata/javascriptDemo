e83d459d5424b6e0b218de8793785e37d9d66242
Asavei Florin <asaveiflorin@gmail.com>
2017-12-03 12:04:34 +0200

    hello world in Vue

--- Playground/VueJS/Tutorial/Udemy/TODOapp/src/index.js
+++ Playground/VueJS/Tutorial/Udemy/TODOapp/src/index.js
@@ -4,21 +4,6 @@ import App from './App'
 
 new Vue({
     el: '#app',
-    template: `<App :state="state" />`,
-    components: { App },
-    data:{
-        state:{
-            showDone: false,
-            todos:[{
-                text: "Refactor CSS",
-                done: true,
-            },{
-                text: "Secure Docker Instances",
-                done: false
-            },{
-                text: "Inject flavoring",
-                done: false
-            }]
-        }
-    }
+    template: `<App />`,
+    components: { App }
 });
