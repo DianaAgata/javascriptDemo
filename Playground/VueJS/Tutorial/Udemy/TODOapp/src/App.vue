7a185ba3e2d5f0544265d7edfc8963b80e6d4f14
Asavei Florin <asaveiflorin@gmail.com>
2017-12-03 13:15:09 +0200

    using v-bind

--- Playground/VueJS/Tutorial/Udemy/TODOapp/src/App.vue
+++ Playground/VueJS/Tutorial/Udemy/TODOapp/src/App.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
         <ul v-for="todo in $data.todos">
-            <li>{{todo.text}}</li>
+            <li v-bind:class="{complete: todo.done}">
+                {{todo.text}}
+                <input type="checkbox" v-model="todo.done">
+            </li>
         </ul>
+        <form v-on:submit="handleAddTodo($event)">
+            <input type="text" v-model="input">
+        </form>
     </div>
 </template>
 <script>
     export default {
+        methods: {
+            handleAddTodo(e) {
+                e.preventDefault();
+                this.$data.todos.push({text: this.input, done: false});
+                this.input = "";
+            }
+        },
         data() {
             return this.$props.state;
         },
@@ -14,4 +27,9 @@
     }
 </script>
 <style>
+    .complete {
+        color: gainsboro;
+        text-decoration: line-through;
+        font-style: italic;
+    }
 </style>
