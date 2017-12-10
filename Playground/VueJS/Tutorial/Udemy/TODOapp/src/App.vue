c58f8f3c22c62066121a71b4f19331c360551fc2
Asavei Florin <asaveiflorin@gmail.com>
2017-12-10 11:18:05 +0200

    finished VuejS todo APP

--- Playground/VueJS/Tutorial/Udemy/TODOapp/src/App.vue
+++ Playground/VueJS/Tutorial/Udemy/TODOapp/src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <ul v-for="todo in $data.todos">
+        <h1>VueJs TODO app</h1>
+        <ul v-for="todo in $data.todos.filter(shouldShowTodo)">
             <li v-bind:class="{complete: todo.done}">
                 {{todo.text}}
                 <input type="checkbox" v-model="todo.done">
@@ -9,6 +10,10 @@
         <form v-on:submit="handleAddTodo($event)">
             <input type="text" v-model="input">
         </form>
+        <label class="showCompleted">
+            Show completed items?
+            <input type="checkbox" v-model="showDone">
+        </label>
     </div>
 </template>
 <script>
@@ -18,6 +23,13 @@
                 e.preventDefault();
                 this.$data.todos.push({text: this.input, done: false});
                 this.input = "";
+            },
+            shouldShowTodo(todo) {
+                if (this.$data.showDone) {
+                    return true;
+                } else {
+                    return !todo.done;
+                }
             }
         },
         data() {
@@ -27,9 +39,61 @@
     }
 </script>
 <style>
+    body {
+        font-style: Helvetica, sans-serif;
+        color: darkslategray;
+        font-size: 1.4em;
+        margin: 1.4em;
+        width: 22em;
+        position: relative;
+    }
+
+    input[type=text] {
+        margin-top: 1em;
+        padding: 0.8em;
+        width: 100%;
+        font-size: 0.7em;
+    }
+
+    input[type=checkbox] {
+        position: absolute;
+        right: 0;
+        padding: 1em;
+        cursor: pointer;
+    }
+
+    li{
+        list-style-type: none;
+        padding: 1em 0 1em 0;
+        border-bottom: 1px solid lightblue;
+        position: relative;
+    }
+
+    li input{
+        transform: scale(1.8);
+    }
+
+    ul{
+        padding: 0;
+    }
+
+    label{
+        widht:80%
+    }
+
     .complete {
         color: gainsboro;
         text-decoration: line-through;
         font-style: italic;
     }
+
+    .showCompleted{
+        font-size: 1rem;
+        color:slategray;
+    }
+
+    .showCompleted input{
+        transform: scale(1.4);
+    }
+
 </style>
