3c9349ed7ae0f6c0382097cf14b3844167151b7e
Florin Asavei <asaveiflorin@gmail.com>
2018-01-16 12:48:47 +0200

    add panzoom plugin

--- Playground/_otherLibraries/diagrams/03_cytoscape.js/examples/__POC/02/code.js
+++ Playground/_otherLibraries/diagrams/03_cytoscape.js/examples/__POC/02/code.js
@@ -233,6 +233,8 @@ var cy = window.cy = cytoscape({
             selector: '.node.rejected',
             css: {
                 'background-color': 'red',
+                'border-color': "blue",
+                'border-width': "2px",
                 'line-color': 'black',
                 'target-arrow-color': 'black',
                 'source-arrow-color': 'black'
@@ -265,7 +267,37 @@ var cy = window.cy = cytoscape({
 
 
 cy.minZoom(0.5);
-cy.maxZoom(2);
+cy.maxZoom(5);
+
+
+var defaults = {
+    zoomFactor: 0.05, // zoom factor per zoom tick
+    zoomDelay: 45, // how many ms between zoom ticks
+    minZoom: 0.5, // min zoom level
+    maxZoom: 5, // max zoom level
+    fitPadding: 50, // padding when fitting
+    panSpeed: 10, // how many ms in between pan ticks
+    panDistance: 10, // max pan distance per tick
+    panDragAreaSize: 75, // the length of the pan drag box in which the vector for panning is calculated (bigger = finer control of pan speed and direction)
+    panMinPercentSpeed: 0.25, // the slowest speed we can pan by (as a percent of panSpeed)
+    panInactiveArea: 8, // radius of inactive area in pan drag box
+    panIndicatorMinOpacity: 0.5, // min opacity of pan indicator (the draggable nib); scales from this to 1.0
+    zoomOnly: false, // a minimal version of the ui only with zooming (useful on systems with bad mousewheel resolution)
+    fitSelector: undefined, // selector of elements to fit
+    animateOnFit: function(){ // whether to animate on fit
+        return false;
+    },
+    fitAnimationDuration: 1000, // duration of animation on fit
+
+    // icon class names
+    sliderHandleIcon: 'fa fa-minus',
+    zoomInIcon: 'fa fa-plus',
+    zoomOutIcon: 'fa fa-minus',
+    resetIcon: 'fa fa-expand'
+};
+
+cy.panzoom( defaults );
+
 
 //center the graph on a node
 // var j = cy.$("#j");
